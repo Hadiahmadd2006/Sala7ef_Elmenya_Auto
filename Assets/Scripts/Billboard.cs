06285e4ea6473a8e3d6bda8dff3a9a025854ba27
Hadiahmadd2006 <ghareeb.hadi1@gmail.com>
2026-05-19 19:49:34 +0300

    BMW door animations

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
-
 public class Billboard : MonoBehaviour
 {
     void LateUpdate()
     {
         if (Camera.main == null) return;
-
         transform.LookAt(Camera.main.transform);
-
-        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 180, 0);
+        transform.Rotate(0f, 180f, 0f);
     }
 }
