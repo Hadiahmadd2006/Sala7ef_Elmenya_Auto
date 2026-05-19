327558219037820f3feefafe24da54ad74bf64a8
Hadiahmadd2006 <ghareeb.hadi1@gmail.com>
2026-05-20 00:00:57 +0300

    Update Billboard.cs

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -2,16 +2,11 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    private Camera cam;
-
     void LateUpdate()
     {
-        if (cam == null)
-            cam = Camera.main;
-        if (cam == null)
-            return;
-
-        transform.LookAt(cam.transform);
-        transform.Rotate(0f, 180f, 0f);
+        if (Camera.main != null)
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
     }
 }
