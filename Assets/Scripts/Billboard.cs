93114ce3af655941d38bd43f64c92ec8909dbc5d
Hadiahmadd2006 <ghareeb.hadi1@gmail.com>
2026-05-19 22:39:06 +0300

    Test number 1

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
+
 public class Billboard : MonoBehaviour
 {
+    private Camera cam;
+
     void LateUpdate()
     {
-        if (Camera.main == null) return;
-        transform.LookAt(Camera.main.transform);
+        if (cam == null)
+            cam = Camera.main;
+        if (cam == null)
+            return;
+
+        transform.LookAt(cam.transform);
         transform.Rotate(0f, 180f, 0f);
     }
 }
